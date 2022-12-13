c6f3651da53ef4fc30cd6b6ce5100765f920cb9b
pateljasmin10.12 <pateljasmin10.12@gmail.com>
2022-12-13 14:14:39 +0000

    Robot wars project commited.

---
+++ RobotWars/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RobotWars.Application.Logger;
+using RobotWars.Application.Creators;
+using RobotWars.Application.Entity;
+using RobotWars.Application.CommandProcess;
+
+ServiceProvider serviceProvider = new ServiceCollection()
+    .AddLogging((loggingBuilder) => loggingBuilder
+        .SetMinimumLevel(LogLevel.Trace)
+        .AddConsole()
+        ).BuildServiceProvider();
+
+
+
+
+var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+{
+    services.AddTransient<IConsoleLogger, CommandLineLogger>();
+    services.AddTransient<IRobotProcess, RobotProcess>();
+    services.AddTransient<IArenaProcess, ArenaProcess>();
+    services.AddTransient<IRobotBuilder, RobotBuilder>();
+    services.AddTransient<IArenaBuilder, ArenaBuilder>();
+    services.AddTransient<IRobot, Robot>();
+    services.AddTransient<IArena, Arena>();
+
+}).Build();
+
+
+var IArena = host.Services.GetService<IArena>();
+var IArenaProcessor = host.Services.GetService<IArenaProcess>();
+var IARenaBuilder = host.Services.GetService<IArenaBuilder>();
+
+
+
+Console.WriteLine("Type Arena size (ex: 5 5)");
+
+string arenaCreateCommand = Console.ReadLine();
+IArena _IArena = IArenaProcessor.ProcessCommand(arenaCreateCommand);
+
+
+string robotCreateCommand = string.Empty;
+string robotMovementCommand = string.Empty;
+///Get Inpt for Robot1
+
+if (_IArena != null)
+{
+    var IRobotProcess1 = host.Services.GetService<IRobotProcess>();
+    if (IRobotProcess1 != null)
+    {
+        Console.WriteLine("Type Robot 1 location (ex: 1 2 N)");
+        robotCreateCommand = Console.ReadLine();
+        IRobotProcess1.CreateCommand(robotCreateCommand, _IArena);
+        Console.WriteLine("Type Robot 1 movements (ex: LMLRM)");
+        robotMovementCommand = Console.ReadLine();
+        IRobotProcess1.ProcessCommand(robotMovementCommand);
+        IRobotProcess1.DisplayRobotStatus();
+    }
+
+    var IRobotProcess2 = host.Services.GetService<IRobotProcess>();
+    if (IRobotProcess2 != null)
+    {
+        Console.WriteLine("Type Robot 2 location (ex: 1 2 N)");
+        robotCreateCommand = Console.ReadLine();
+        IRobotProcess2.CreateCommand(robotCreateCommand, _IArena);
+        Console.WriteLine("Type Robot 2 movements (ex: LMLRM)");
+        robotMovementCommand = Console.ReadLine();
+        IRobotProcess2.ProcessCommand(robotMovementCommand);
+        IRobotProcess2.DisplayRobotStatus();
+    }
+
+}
+
+Console.ReadKey();
+
+
+
+
+
+
+
---
+++ RobotWars/UtilityClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotWars.Console
+{
+    public class UtilityClass
+    {
+        public static IEnumerable Parse(string input)
+        {
+            return input.Select(c => c);
+        }
+    }
+}
